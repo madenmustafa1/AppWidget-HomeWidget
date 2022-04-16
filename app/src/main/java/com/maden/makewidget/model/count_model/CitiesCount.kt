ce356faf925e2f1339d51fcd157b5270f62662d6
madenmustafa <madenn778@gmail.com>
2022-04-16 15:29:56 +0300

    .

--- app/src/main/java/com/maden/makewidget/model/count_model/CitiesCount.kt
+++
@@ -1,6 +0,0 @@
-package com.maden.makewidget.model.count_model
-
-data class CitiesCount(
-    val cityName: String,
-    val workOrderCount: Int
-)
--- app/src/main/java/com/maden/makewidget/model/count_model/CountModel.kt
+++
@@ -1,9 +0,0 @@
-package com.maden.makewidget.model.count_model
-
-data class CountModel(
-    val citiesCounts: List<CitiesCount>?,
-    val courierCount: Int?,
-    val customerCount: Int?,
-    val partnerCount: Int?,
-    val workOrderCount: WorkOrderCount?
-)
